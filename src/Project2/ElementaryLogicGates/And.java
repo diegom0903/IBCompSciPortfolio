5a33ff83058f4b93feea56eb98bfbc7d77954191
jander15 <45211598+jander15@users.noreply.github.com>
2020-10-28 11:19:26 -0600

    josh and diego session on Not and And Gates

--- src/Project2/ElementaryLogicGates/And.java
+++ src/Project2/ElementaryLogicGates/And.java
@@ -1,13 +1,18 @@
 package Project2.ElementaryLogicGates;
 
 public class And {
-
     public boolean out;
+    public Nand nand1;
+    public Not not1;
 
     public And() {
+        nand1 = new Nand();
+        not1 = new Not();
     }
 
     public void compute(boolean a, boolean b){
-
+        nand1.compute(a,b);
+        not1.compute(nand1.out);
+        out=not1.out;
     }
 }
--- src/Project2/ElementaryLogicGates/Not.java
+++ src/Project2/ElementaryLogicGates/Not.java
@@ -2,13 +2,15 @@ package Project2.ElementaryLogicGates;
 
 public class Not {
     public boolean out;
+    public Nand nand1;
 
     public Not() {
-
+        nand1=new Nand();
     }
 
     public void compute(boolean in){
-
+        nand1.compute(in,in);
+        out=nand1.out;
     }
 
 }
