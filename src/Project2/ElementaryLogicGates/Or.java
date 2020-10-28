ede2ddfa3ad60aee7446b7140846bf0bd6845165
diegom0903 <72108644+diegom0903@users.noreply.github.com>
2020-10-28 11:31:25 -0600

    Or Gate

--- src/Project2/ElementaryLogicGates/Or.java
+++ src/Project2/ElementaryLogicGates/Or.java
@@ -1,15 +1,25 @@
 package Project2.ElementaryLogicGates;
 
 public class Or {
-
     public boolean out;
+    private Not not1;
+    private Not not2;
+    private Not not3;
+    private And and1;
 
     public Or() {
-
+    not1 = new Not();
+    not2 = new Not();
+    not3 = new Not();
+    and1 = new And();
     }
 
     public void compute(boolean a, boolean b){
-
+        not1.compute(a);
+        not2.compute(b);
+        and1.compute(not1.out, not2.out);
+        not3.compute(and1.out);
+        out=not3.out;
     }
 
 }
