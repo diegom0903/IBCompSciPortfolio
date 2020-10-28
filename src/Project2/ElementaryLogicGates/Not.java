61cf8414e33848cf4f725e864d9d452893d45fb7
jander15 <45211598+jander15@users.noreply.github.com>
2020-10-28 11:23:01 -0600

    changed name of nand gate in the Not gate implementation

--- src/Project2/ElementaryLogicGates/Not.java
+++ src/Project2/ElementaryLogicGates/Not.java
@@ -2,15 +2,15 @@ package Project2.ElementaryLogicGates;
 
 public class Not {
     public boolean out;
-    public Nand nand1;
+    public Nand nand;
 
     public Not() {
-        nand1=new Nand();
+        nand=new Nand();
     }
 
     public void compute(boolean in){
-        nand1.compute(in,in);
-        out=nand1.out;
+        nand.compute(in,in);
+        out=nand.out;
     }
 
 }
